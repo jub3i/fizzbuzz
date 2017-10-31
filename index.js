fa0e96d2a59caa6caa1454142fb2fca24d6a57ba
jubei <anon66@gmail.com>
2017-10-31 21:55:33 +0200

    add root route

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 5000
 
 app.use('/public', express.static('./public'))
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
+app.get('/', function (req, res) {
+  res.send(fs.readFileSync('./public/index.html', 'utf8'))
+})
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
